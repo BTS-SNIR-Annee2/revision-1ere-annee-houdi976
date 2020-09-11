a3030b1e44ea07a72a0cd9db50a2baf38c522193
houdi <houdi.rafion@lyceeppr.fr>
2020-09-11 15:28:53 +0200

    question3

---
+++ question3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int A;
+	int C;
+	cout<<"entrez un nombre"<<endl;
+	cin >> A;
+	
+	C=A*A;
+
+	cout<< "le carré de A est  "<< C << endl;
+	return 0;
+}
+
